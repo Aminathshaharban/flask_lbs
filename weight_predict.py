be372a3b12af1d6cf27d1fc93865be95714411c8
Aminathshaharban <142972507+Aminathshaharban@users.noreply.github.com>
2023-08-23 10:34:53 +0000

    app complete

---
+++ weight_predict.py
@@ -0,0 +1,9 @@
+import pickle
+
+def prediction(height):
+    pickled_model =pickle.load(open('model.pkl','rb'))
+    weight =pickled_model.predict(height)
+    print(weight)
+    print(height)
+    return weight
+    
